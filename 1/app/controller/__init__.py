ced2bff9fae9872617553fcf0ec5b83a09aa4b34
vfasky <vfasky@gmail.com>
2012-02-01 17:48:54 +0800

    upload

--- 1/app/controller/__init__.py
+++ 1/app/controller/__init__.py
@@ -1,2 +1,27 @@
+#coding=utf-8
 __author__ = 'vfasky'
-  
+
+'''
+实现插件机制
+'''
+
+import functools
+# 执行控制器动作之前调用
+def beforeExecute(method):
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+
+        #print self.request.method
+        print str(self.__class__.__module__)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+# 渲染之前调用
+def beforeRender(method):
+    @functools.wraps(method)
+    def wrapper(self, template_name, **kwargs):
+
+        return method(self, template_name, **kwargs)
+
+    return wrapper
